1bec762ce9a50db2d505bbe15358afa0061e21c7
boanli <boan.li@asu.edu>
2025-02-13 19:26:41 -0700

    solve conflict

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -36,13 +36,10 @@ router.get('/professor/:username/classes', verifyToken, getClassesForProfessor);
 router.post('/class/assign', verifyToken, assignProfessorToClass);
 router.post('/class/unassign', verifyToken, unassignProfessorFromClass);
 
-
-
 router.get('/student/:id', verifyToken, getStudent);
 router.post('/student', verifyToken, createStudent);
 router.put('/student/:id', verifyToken, updateStudent);
 router.delete('/student/:id', verifyToken, deleteStudent);
-
 
 router.get('/log/:id', verifyToken, getLog);
 router.post('/log', verifyToken, createLog);
