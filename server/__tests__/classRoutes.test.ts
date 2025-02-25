7c80dc1d3ccbb3e064053fb58a5523e144ff24a7
Alvin Tran <atran50@asu.edu>
2025-02-24 19:27:22 -0700

    Add tests for paginated GET classes

--- server/__tests__/classRoutes.test.ts
+++ server/__tests__/classRoutes.test.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import routes from '../src/routes/index';
 import DatabaseAccess from '../src/services/database';
 import AuthService from '../src/services/auth';
-import { expect, test, describe, beforeAll } from '@jest/globals';
+import { expect, test, describe, beforeAll, beforeEach } from '@jest/globals';
 
 const app = express();
 app.use(express.json());
@@ -17,7 +17,6 @@ describe('Class API', () => {
     AuthService.init();
     db = await DatabaseAccess.getInstance();
 
-    await db.runWithNoReturned('DELETE FROM class');
     await db.runWithNoReturned('DELETE FROM user');
     await db.runWithNoReturned('DELETE FROM credential');
 
@@ -27,6 +26,10 @@ describe('Class API', () => {
     if (!token) {
       throw new Error('Failed to generate admin token');
     }
+  });
+
+  beforeEach(async () => {
+    await db.runWithNoReturned('DELETE FROM class');
   });
 
   test('POST /api/class should create a class successfully', async () => {
@@ -126,5 +129,62 @@ describe('Class API', () => {
       'error',
       "Class with id 'invalidId' not found"
     );
+  });
+
+  test('GET /api/classes should return 200 and 1 page of 2 classes', async () => {
+    await request(app)
+      .post('/api/class')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'SER517',
+      });
+
+    await request(app)
+      .post('/api/class')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'CSE546',
+      });
+
+    const response = await request(app)
+      .get(`/api/classes`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('page', 1);
+    expect(response.body).toHaveProperty('page_size', 10);
+    expect(response.body).toHaveProperty('total_items', 2);
+    expect(response.body).toHaveProperty('total_pages', 1);
+    expect(response.body.data).toHaveLength(2);
+    expect(response.body.data[0]).toHaveProperty('name', 'SER517');
+    expect(response.body.data[1]).toHaveProperty('name', 'CSE546');
+  });
+
+  test('GET /api/classes?page=2&size=1 should return 200 and 1 page of 1 singular class', async () => {
+    await request(app)
+      .post('/api/class')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'SER517',
+      });
+
+    await request(app)
+      .post('/api/class')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'CSE546',
+      });
+
+    const response = await request(app)
+      .get(`/api/classes?page=2&size=1`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('page', 2);
+    expect(response.body).toHaveProperty('page_size', 1);
+    expect(response.body).toHaveProperty('total_items', 2);
+    expect(response.body).toHaveProperty('total_pages', 2);
+    expect(response.body.data).toHaveLength(1);
+    expect(response.body.data[0]).toHaveProperty('name', 'CSE546');
   });
 });
