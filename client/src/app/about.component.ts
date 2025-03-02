e32407253e80ee6452393524c5f7c874bc3cf757
boanli <boan.li@asu.edu>
2025-03-02 07:21:00 -0700

    almost finish UI design, will fix the Angular CLI outside problem later

---
+++ client/src/app/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  template: `<p>This is the about page.</p>`,
+  standalone: true,
+})
+export class AboutComponent {}
---
+++ client/src/app/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<p>This is the home page.</p>`,
+  standalone: true,
+})
+export class HomeComponent {}
