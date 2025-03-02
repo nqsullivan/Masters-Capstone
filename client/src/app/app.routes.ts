f29375c95f9adf90b2cf03d77a59c1ca4ecd7865
boanli <boan.li@asu.edu>
2025-03-02 08:35:43 -0700

    finished the UI skeleton, size can be changed to fix future branch, 3 example button which can be clicked. I will add some example page for each button later.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
+import { ContactComponent } from './contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
