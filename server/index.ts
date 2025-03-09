7c61ccba0a8915b4586c25de140bd2c8260b01a8
Nathaniel Sullivan <nathaniel.sullivan@geminidsystems.com>
2025-03-09 16:48:56 -0700

    Adding cors to rest api

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './src/routes/index.js';
 import AuthService from './src/services/auth.js';
 import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 AuthService.init();
+
+app.use(
+  cors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,8 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 
 app.listen(port, () => {
-  dotenv.config();
   console.log(`Server is running on port ${port}`);
-  // Example on how to grab environment variables:
   console.log(`S3 Bucket Name: ${process.env.BUCKET_NAME}`);
 });
