26bea3c49010c1c96df135c7c778fe82ac8d0572
Nathaniel Sullivan <nathaniel.sullivan@geminidsystems.com>
2025-03-09 23:03:50 -0700

    Working dashboard implementation

---
+++ client/src/app/models/models.ts
@@ -0,0 +1,35 @@
+export interface Class {
+  id: string;
+  name: string;
+}
+
+export interface Session {
+  id: string;
+  startTime: string;
+  endTime: string;
+  classId: string;
+  professorId: string;
+}
+
+export interface Student {
+  id: string;
+  name: string;
+  image: string;
+}
+
+export interface AttendanceRecord {
+  id: string;
+  studentId: string;
+  sessionId: string;
+  checkIn: string;
+  portaitUrl: string;
+  portait_captured: boolean;
+}
+
+export interface DashboardData {
+  class: Class;
+  professor: string;
+  students: Student[];
+  sessions: Session[];
+  attendance: Map<string, AttendanceRecord[]>; // sessionId -> AttendanceRecord[]
+}
