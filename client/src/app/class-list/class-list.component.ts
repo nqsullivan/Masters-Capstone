81b17e3dafd7265358fae33f3f9ad10b000ef259
Alvin Tran <atran50@asu.edu>
2025-03-17 12:58:49 -0700

    Implement fully working class list component

--- client/src/app/class-list/class-list.component.ts
+++ client/src/app/class-list/class-list.component.ts
@@ -2,7 +2,7 @@
  * Sourced from https://material.angular.io/components/table/examples
  */
 
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
@@ -11,45 +11,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ApiService } from '../services/api.service';
 import { Class } from '../models/models';
 
-export interface UserData {
+export interface ClassListData {
   id: string;
   name: string;
-  progress: string;
-  fruit: string;
 }
-
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
 
 /**
  * @title Data table with sorting, pagination, and filtering.
@@ -66,30 +31,23 @@ const NAMES: string[] = [
     MatPaginatorModule,
   ],
 })
-export class ClassListComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+export class ClassListComponent {
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource: MatTableDataSource<ClassListData>;
   classes: Class[] = [];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private apiService: ApiService) {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
-
     this.apiService.get<{ data: Class[] }>('classes').subscribe((data) => {
-      console.log(data.data);
       this.classes = data.data;
-    });
-  }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+      // Assign the data to the data source for the table to render
+      this.dataSource = new MatTableDataSource(this.classes);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
   }
 
   applyFilter(event: Event) {
@@ -100,20 +58,4 @@ export class ClassListComponent implements AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
   }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
 }
