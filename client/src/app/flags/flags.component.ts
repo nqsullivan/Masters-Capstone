1b1895f4afc3bdcce6a1c8615ef39c7d9b945c65
Nathaniel Sullivan <nathaniel.sullivan@geminidsystems.com>
2025-04-04 15:03:34 -0700

    Add attendance flags component and routing

---
+++ client/src/app/flags/flags.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flags',
+  imports: [],
+  templateUrl: './flags.component.html',
+  styleUrl: './flags.component.css',
+})
+export class FlagsComponent {}
